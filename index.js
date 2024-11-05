b603a695efe0f3cbf8db00f8aab6db7db9ed71a8
Ahmad8881 <1221mohammadahmad@gmail.com>
2024-11-05 12:14:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,221 @@
+
+
+// ans Number1
+
+// function countCharacters() {
+
+//   let countA = 0;
+//   let countD = 0;
+
+//   for (let char of S) {
+
+//       if (char === 'A') {
+
+//           countA++;
+
+//       } else if (char === 'D') {
+
+//           countD++;
+//       }
+//   }
+
+//   return [countA, countD];
+// }
+
+// let S = "AbaDd";
+
+// console.log(countCharacters()); 
+
+
+
+// ans3
+
+
+// let str = "prepbytes"
+// let  Vowels = "aeiouAEIOU"
+// let count= 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     for (let j = 0; j < Vowels.length; j++) {
+
+//         if (str[i]===Vowels[j]) {
+//             count++
+
+//         }
+
+//     }
+
+// }
+// console.log(count)
+
+// ans 4
+
+// let S1 = "Prep";
+// let S2 = "bytes";
+
+// let S3 = S1.concat(S2);
+
+// console.log(S3);
+
+// ans 5
+
+// let str1 = "hello world"
+
+// function findLength() {
+//     let Count = 0;
+
+//     for (let i = 0; i < str1.length; i++) {
+
+//             Count++
+
+
+//     }
+//     return Count;
+// }
+
+// let result = findLength()
+// console.log(result)
+
+// ans 6
+
+// let str2 = "ADDAAADDDDD"
+
+// function  Game_Winner() {
+//     for (let i = 0; i < str2.length; i++) {
+//         if (str2 === "A") {
+//             console.log("amit win")
+
+//         }else{
+//             console.log("danish win")
+
+//         }
+
+//     }
+
+// }
+// Game_Winner();
+
+// ans 7
+
+// let a = "prepbyte"
+// let b = "technologies"
+
+// let s = a.concat(b)
+// console.log(s)
+
+
+// ans 8
+
+// const S1 = "Naman";
+// // const S2 = "naman";
+
+// function isPalindrome(S) {
+//     const reversedS = S.split('').reverse().join('');
+//     return S.toLowerCase() === reversedS.toLowerCase();
+//   }
+
+
+
+
+//   console.log(isPalindrome(S1)); 
+// //   console.log(isPalindrome(S2)); 
+
+
+
+// ans 9
+
+
+// let S = "I am utkarsh raj";
+
+// function reverseString(S) {
+
+//     return S.split('').reverse().join('');
+// }
+
+
+
+// let reversedS = reverseString(S);
+// console.log(reversedS);
+
+
+// ans 10
+
+// let s1 = "prepbyte"
+// let s2 = "prepbyte"
+
+// function String_Match() {
+
+//     if (s1===s2) {
+
+//         console.log("YES")
+
+//     }else{
+
+//         console.log("NO")
+
+//     }
+
+// }
+
+// String_Match();
+
+
+// ans 11.
+
+
+// let S = "Hi, I am You. You Prepbytes";
+// let pattern = "You";
+// let text = "Prepbytes";
+
+// function Replace() {
+
+//     return S.replace(new RegExp(pattern, 'g'), text);
+
+// }
+
+
+
+// let result = Replace();
+
+
+// console.log(result);
+
+
+
+// ans 12
+
+// let S = "I am utkarsh raj";
+
+// function Split_the_String() {
+
+//     return S.split(' ').join('\n');
+// }
+
+
+
+// let result = Split_the_String();
+
+
+// console.log(result);
+
+
+// ans 13.
+
+
+// let str4 = "prepbytes";
+// let vowelCount = 0;
+// let consonantCount = 0;
+
+// let vowels = "aeiouAEIOU";
+
+// for (let i = 0; i < str4.length; i++) {
+//   if (vowels.includes(str4[i])) {
+//     vowelCount++;
+//   } else {
+//     consonantCount++;
+//   }
+// }
+
+// console.log(vowelCount, consonantCount);
+
+
